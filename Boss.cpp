a457cc6c70f83ec708bf57385395b982acfcbd7e
Bosskboy <kokazono380@gmail.com>
2024-09-03 06:37:26 +0700

    Ms

---
+++ Boss.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    system("cls");
+    system("color a");
+
+    cout <<"===========| Wellcome |=========="<<endl;
+
+    string name;
+    int id ;
+    cout<<" Please Iput your name :";getline(cin,name);
+    cout<<" Please iput your id : ";cin>>id;
+    cout<<" Your name is >> "<<name<<" << work on security"<<endl;
+    cout <<" your id is "<<id<<endl;
+     
+// love
+
+    return 0;
+}
---
+++ Kboy.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    system("cls");
+    system("color a");
+
+    cout <<"===========| Wellcome |=========="<<endl;
+
+   float salary;
+   cout<<" Pleas input your salary ";cin>>salary;
+   cout<<" Your salary is :"<<salary<<endl;
+     
+
+    return 0;
+}
